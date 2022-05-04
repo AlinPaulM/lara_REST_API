cd331d961654a30d82a89e7593aa9a0b74dcc2b7
Alin M <macoveialinp@gmail.com>
2022-05-04 15:12:29 +0100

    ImageManipulation migration code

--- database/migrations/2022_05_04_135812_create_image_manipulations_table.php
+++ database/migrations/2022_05_04_135812_create_image_manipulations_table.php
@@ -15,7 +15,15 @@ return new class extends Migration
     {
         Schema::create('image_manipulations', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name', 255);
+            $table->string('path', 2000);
+            $table->string('type', 25);
+            $table->text('data');
+            $table->string('output_path', 2000);
+            $table->timestamp('created_at');
+
+            $table->foreignIdFor(\App\Models\User::class, 'user_id');
+            $table->foreignIdFor(\App\Models\Album::class, 'album_id');
         });
     }
 

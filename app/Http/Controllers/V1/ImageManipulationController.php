e7f392346c84eceb467ca8eabbb887ddbe2be6d9
Alin M <macoveialinp@gmail.com>
2022-05-13 16:32:34 +0300

    implemented image manipulation

--- app/Http/Controllers/V1/ImageManipulationController.php
+++ app/Http/Controllers/V1/ImageManipulationController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Models\ImageManipulation;
 use App\Models\Album;
 use App\Http\Requests\ResizeImageRequest;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\Facades\Image;
 
 class ImageManipulationController extends Controller
 {
@@ -27,7 +30,64 @@ class ImageManipulationController extends Controller
      */
     public function resize(ResizeImageRequest $request)
     {
-        //
+        $all = $request->all(); 
+        // $validated = $request->validated(); // i'd usually use this, as this retrieves ONLY the validated input data(unlike $request->all() which gets other stuff too like hidden inputs and stuff)
+
+        /** @var UploadedFile|string $image */
+        $image = $all['image'];
+        unset($all['image']); // because later we'll save $all inside image_manipulations table's data column
+
+        $data = [
+            'type' => ImageManipulation::TYPE_RESIZE,
+            'data' => json_encode($all),
+            'user_id' => null
+        ];
+
+        if(isset($all['album_id'])) {
+            // TODO
+            $data['album_id'] = $all['album_id'];
+        }
+
+        # the guy decided to store the images under public/images instead of storage/app/public and make a symlink from public/storage to storage/app/public, like the documentation suggests https://laravel.com/docs/9.x/filesystem#the-public-disk
+
+        // create random dir under public/images
+        $dir = 'images/' . Str::random() . '/'; // this is the relative path
+        $absolutePath = public_path($dir);
+        File::makeDirectory($absolutePath);
+
+        // public/images/asidhf32rhh/test.jpg
+        // public/images/asidhf32rhh/test-resized.jpg
+        if($image instanceof \Illuminate\Http\UploadedFile) {
+            $data['name'] = $image->getClientOriginalName();            
+            $filename = pathinfo($data['name'], PATHINFO_FILENAME);
+            $extension = $image->getClientOriginalExtension();            
+
+            $image->move($absolutePath, $data['name']);
+        } else { // image is url
+            $data['name'] = pathinfo($image, PATHINFO_BASENAME);
+            $filename = pathinfo($image, PATHINFO_FILENAME);
+            $extension = pathinfo($image, PATHINFO_EXTENSION);
+
+            copy($image, $absolutePath . $data['name']); // https://www.php.net/manual/en/function.copy
+        }
+        $data['path'] = $dir . $data['name'];
+        $originalPath = $absolutePath . $data['name'];
+
+
+        # resize
+        $w = $all['w'];
+        $h = $all['h'] ?? false;
+
+        list($width, $height, $image) = $this->getImageWidthAndHeight($w, $h, $originalPath); // https://www.php.net/manual/en/function.list.php
+
+        $resizedFilenameWithExt = $filename . '-resized.' . $extension;
+        $image->resize($width, $height)->save($absolutePath . $resizedFilenameWithExt);
+        $data['output_path'] = $dir . $resizedFilenameWithExt;
+
+        # create ImageManipulation db record
+        $imageManipulation = ImageManipulation::create($data);
+
+        return $imageManipulation;
     }
 
     /**
@@ -61,5 +121,35 @@ class ImageManipulationController extends Controller
     public function destroy(ImageManipulation $imageManipulation)
     {
         //
+    }
+
+    /**
+     * Get the image's new width and height, in px
+     * 
+     * @param string|float $w can be speficified in px as a number(without 'px') or percentage
+     * @param string|float $h OPTIONAL can be speficified in px as a number(without 'px') or percentage
+     * both $w and $h will be either px or % values, not both(we don't support that case)
+     * @param string $originalPath
+     * 
+     * @return array returns the new width and height in pixels as float numbers(without 'px'), and the image
+     */
+    protected function getImageWidthAndHeight($w, $h, string $originalPath)
+    {
+        $image = Image::make($originalPath);
+        $originalWidth = $image->width();
+        $originalHeight = $image->height();
+
+        if(str_ends_with($w, '%')) {
+            $ratioW = (float)str_replace('%', '', $w);
+            $ratioH = $h ? (float)str_replace('%', '', $h) : $ratioW;
+
+            $newWidth = $originalWidth * $ratioW / 100;
+            $newHeight = $originalHeight * $ratioH / 100;
+        } else {
+            $newWidth = (float)$w;
+            $newHeight = $h ? (float) $h : $originalHeight * $newWidth / $originalWidth;
+        }
+
+        return [$newWidth, $newHeight, $image];
     }
 }
--- app/Models/ImageManipulation.php
+++ app/Models/ImageManipulation.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class ImageManipulation extends Model
 {
     use HasFactory;
+
+    const UPDATED_AT = null; // cuz we don't have an 'updated_at' column and we'd get an error otherwise. https://laravel.com/docs/9.x/eloquent#timestamps
+    const TYPE_RESIZE = 'resize';
+
+    protected $fillable = ['name', 'path', 'type', 'data', 'output_path', 'user_id', 'album_id'];
+    // protected $guarded = [];
 }
